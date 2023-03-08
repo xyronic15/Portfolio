630ee920110aca8cf8d07ca1b966c134598827a5
Xyron Brual <xyronb@gmail.com>
2023-03-07 21:37:42 -0500

    Added other key components

--- xyron-brual/src/App.js
+++ xyron-brual/src/App.js
@@ -1,25 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This has been changed v2
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
-      <h1 className='text-3xl font-bold underline'>This is a test of the tailwind framework</h1>
+      <Navbar />
     </div>
   );
 }
