6cb40bf3fd23912b8ff72e88395d14f7f68f12cd
Xyron Brual <xyronb@gmail.com>
2023-03-22 21:41:06 -0400

    Started about page
    - Added About component to App.js
    - Started key structure of the about section

--- xyron-brual/src/components/About.jsx
+++ xyron-brual/src/components/About.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div name="About" className="w-full h-screen">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-screen-lg  w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-[#143aa2]">About</p>
+          </div>
+          <div></div>
+          <div className="">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
