173241cd54736a6488179b51333f8a4d7bda4b4b
Xyron Brual <xyronb@gmail.com>
2023-03-22 21:41:06 -0400

    Started about page
    - Added About component to App.js
    - Started key structure of the about section

--- xyron-brual/src/App.js
+++ xyron-brual/src/App.js
@@ -1,12 +1,13 @@
 // import logo from './logo.svg';
 // import './index.css';
-import { Home, Navbar } from './components';
+import { About, Home, Navbar } from './components';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 }
