54ae5e834107c9e8ae02869a2835a9fe0040fd01
Xyron Brual <xyronb@gmail.com>
2023-09-06 15:34:59 -0400

    Changed hover style on navbar links

--- xyron-brual/app/components/Navbar.jsx
+++ xyron-brual/app/components/Navbar.jsx
@@ -15,14 +15,16 @@ export const Navbar = () => {
             <div className="md:flex items-center justify-between bg-[#010409] py-4 md:px-10 px-7 text-gray-50">
                 {/* logo/name */}
                 <div className="uppercase font-bold text-xl cursor-pointer py-2 ">
-                    Xyron Brual
+                    <Link to="Home" smooth={true} duration={500}>
+                        Xyron Brual
+                    </Link>
                 </div>
 
                 {/* main menu */}
                 <div>
                     <ul className={`md:flex md:items-center ${!open ? 'hidden' : 'mt-2'}`}>
                         {Links.map((link) => (
-                            <li key={link.name} className="text-gray-400 cursor-pointer rounded-md px-3 py-2 hover:bg-[#191b22] hover:font-medium hover:text-gray-50 hover:border-b-2 border-gray-50">
+                            <li key={link.name} className="text-gray-400 cursor-pointer px-3 py-2 hover:bg-[#191b22]  hover:text-gray-50 hover:border-b-[1px] border-gray-50">
                                 <Link to={link.name} smooth={true} duration={500} className="">
                                     {link.name}
                                 </Link>
